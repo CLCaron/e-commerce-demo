3a8b733b9e8c5fa4f59749f8312e66b991a8b2d1
christophercaron <christopher.caron@snhu.edu>
2021-09-02 10:57:16 -0400

    Separated app into client folder. Added express server to complete stripe payments with backend

---
+++ client/src/components/stripe-checkout-button/stripe-checkout-button.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JTqP7FBtZD9tTiaNIyMnssb2H0r47UiFxPYQDx7IG7621Yhtgvhk4XuyXFlEnaAvZSgOewGOLXuJT89Ym9MD85k00RglCKIcQ';
+
+  const onToken = (token) => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful!');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided test credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Demo Store Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
