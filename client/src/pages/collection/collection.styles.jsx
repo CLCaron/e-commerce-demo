516c1f1bfe129193f12b7a043aee91f621f803ed
christophercaron <christopher.caron@snhu.edu>
2021-09-09 17:01:59 -0400

    including collection.styles for new header scss

--- client/src/pages/collection/collection.styles.jsx
+++ client/src/pages/collection/collection.styles.jsx
@@ -6,14 +6,31 @@ export const CollectionPageContainer = styled.div`
 `;
 
 export const CollectionTitle = styled.h2`
+  display: flex;
   font-size: 38px;
   margin: 0 auto 30px;
+  padding-left: 180px;
+`;
+
+export const CollectionSortButtonContainer = styled.p`
+  top: 0;
+  right: 0;
+`;
+
+export const CollectionHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  padding-right: 20px;
 `;
 
 export const CollectionItemsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 10px;
+  padding-left: 15px;
   & > div {
     margin-bottom: 30px;
   }
